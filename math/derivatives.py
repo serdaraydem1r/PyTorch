6b73cbcc7f84fbf6fd5aa7f0f07c2980fea72c15
Fatih  Serdar Aydemir <serdaraydemr@gmail.com>
2024-09-08 11:32:19 +0300

    .

---
+++ math/derivatives.py
@@ -0,0 +1,21 @@
+#%%
+import numpy as np
+import sympy as sym
+from IPython.display import display
+
+#%%
+x = sym.Symbol('x')
+fx = 2*x**2
+gx = 4*x**3 - 3*x**4
+
+df = sym.diff(fx)
+dg = sym.diff(gx)
+
+manual = df*gx + fx*dg
+thewrongway = df*dg
+
+viasympy = sym.diff(fx*gx)
+print('The Functions:')
+display(fx)
+display(gxaaaaaaa)
+print(' ')
